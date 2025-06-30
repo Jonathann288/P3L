1bdf270b05ff397ade61eec6b3e5e0d8f213c3fe
lanamarcel1681 <220711681@students.uajy.ac.id>
2025-06-30 16:55:10 +0700

    push

--- app/Models/RequestDonasi.php
+++ app/Models/RequestDonasi.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Requestdonasi extends Model
+class RequestDonasi extends Model
 {
     protected $table = 'requestdonasi';
     protected $primaryKey = 'id_request';
